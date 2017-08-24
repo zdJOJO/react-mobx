571169cb3d763465aa664b0450cb2df67d842009
zdJOJO <461863156@qq.com>
2017-08-24 17:28:08 +0800

    新增user.less

--- src/pages/page1/user.jsx
+++ src/pages/page1/user.jsx
@@ -4,7 +4,7 @@ class User extends Component{
   render(){
     return(                 
       <div>
-        <h4>我的id 是: ${this.props.params.userId}</h4>
+        <h4 className="userName">我的id 是: ${this.props.params.userId}</h4>
       </div>
     );
   }
